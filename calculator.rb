b379294f7044df8ac735841c89fc1f95c1c38f76
Matthew O'Brien <matthew.kevin.obrien@gmail.com>
2021-03-03 15:13:19 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,3 +2,8 @@
 first_number =100
 second_number = 5
 sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
+
+
